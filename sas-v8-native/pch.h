c395de0689f0b345f743e7abd94b123c50b45f9b
zeshan0727 <Zeshanbarvi@gmail.com>
2026-09-16 12:43:10 -0700

    Add native v8 precompiled header

---
+++ sas-v8-native/pch.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+#undef GetCurrentTime
+#include <wincrypt.h>
+#include <shlobj.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+#include <filesystem>
+#include <fstream>
+#include <string>
