9cf256363040936ccbe2838c9c2e2473ab9edf26
zeshan0727 <Zeshanbarvi@gmail.com>
2026-09-20 12:17:15 -0700

    Expose Swift-friendly v0.7.7 split workspace API

--- projects/NextAgent/v0.7.7/NAScreenBridgeClient.h
+++ projects/NextAgent/v0.7.7/NAScreenBridgeClient.h
@@ -6,6 +6,7 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSDictionary *)captureScreen;
 + (NSDictionary *)openSplitWorkspaceWithPrimaryBundleID:(NSString *)primary
                                        secondaryBundleID:(NSString *)secondary;
++ (NSDictionary *)openSplitWorkspaceWithSecondaryBundleID:(NSString *)secondary NS_SWIFT_NAME(openSplitWorkspace(secondary:));
 + (NSDictionary *)splitWorkspaceStatus;
 + (NSDictionary *)closeSplitWorkspace;
 @end
