18497b9f24ff054af95317dd7882264ec6fd0b57
zeshan0727 <Zeshanbarvi@gmail.com>
2026-09-20 22:31:24 -0700

    Expose v0.7.8 SpringBoard OCR and split capability APIs

--- projects/NextAgent/v0.7.7/NAScreenBridgeClient.h
+++ projects/NextAgent/v0.7.7/NAScreenBridgeClient.h
@@ -4,6 +4,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NAScreenBridgeClient : NSObject
 + (NSDictionary *)captureScreen;
++ (NSDictionary *)ocrScreenFast:(BOOL)fast
+                       languages:(NSArray<NSString *> *)languages
+                        maxItems:(NSInteger)maxItems NS_SWIFT_NAME(ocrScreen(fast:languages:maxItems:));
++ (NSDictionary *)splitCapabilities NS_SWIFT_NAME(splitCapabilities());
 + (NSDictionary *)openSplitWorkspaceWithPrimaryBundleID:(NSString *)primary
                                        secondaryBundleID:(NSString *)secondary;
 + (NSDictionary *)openSplitWorkspaceWithSecondaryBundleID:(NSString *)secondary NS_SWIFT_NAME(openSplitWorkspace(secondary:));
