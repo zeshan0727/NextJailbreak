6435fce1e731f892c86c6a56ce76e60852a35179
zeshan0727 <Zeshanbarvi@gmail.com>
2026-09-21 03:09:10 -0700

    Add v0.7.17 hudTest bridge API (header)

--- projects/NextAgent/v0.7.17/NAScreenBridgeClient.h
+++ projects/NextAgent/v0.7.17/NAScreenBridgeClient.h
@@ -15,6 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSDictionary *)closeSplitWorkspace;
 + (NSDictionary *)hidStatus NS_SWIFT_NAME(hidStatus());
 + (NSDictionary *)hudStatus NS_SWIFT_NAME(hudStatus());
++ (NSDictionary *)hudTest NS_SWIFT_NAME(hudTest());
 + (NSDictionary *)hidTapX:(double)x y:(double)y count:(NSInteger)count NS_SWIFT_NAME(hidTap(x:y:count:));
 + (NSDictionary *)hidLongPressX:(double)x y:(double)y duration:(double)duration NS_SWIFT_NAME(hidLongPress(x:y:duration:));
 + (NSDictionary *)hidSwipeX1:(double)x1 y1:(double)y1 x2:(double)x2 y2:(double)y2 duration:(double)duration NS_SWIFT_NAME(hidSwipe(x1:y1:x2:y2:duration:));
