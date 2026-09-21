0d046a5d6eac5f065d042f801d58340af110230a
zeshan0727 <Zeshanbarvi@gmail.com>
2026-09-20 23:02:18 -0700

    Next Post 1.0.14: remove ad initialization and test controls

--- NextPost/Sources/NextPostApp.swift
+++ NextPost/Sources/NextPostApp.swift
@@ -2,19 +2,10 @@ import SwiftUI
 
 @main
 struct NextPostApp: App {
-    init() {
-        AdsManager.shared.initializeIfNeeded()
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.dark)
-                .overlay(alignment: .topTrailing) {
-                    AdTestDeviceButton()
-                        .padding(.top, 8)
-                        .padding(.trailing, 12)
-                }
         }
     }
 }
